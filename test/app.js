3fa73c9ed3ad5f8c18f6e36174a3716cec1e5626
jheel93 <mehta.jh@husky.neu.edu>
2017-06-23 00:08:10 -0400

    basic setup

--- test/app.js
+++ test/app.js
@@ -5,11 +5,11 @@ module.exports = function(app)
     app.delete("/api/test/:id", deleteMessage);
 
     var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-        var password = process.env.MLAB_PASSWORD_WEBDEV;
+    if(process.env.USERNAME) { // check if running remotely
+        var username = process.env.USERNAME; // get from environment
+        var password = process.env.PASSWORD;
         connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+        connectionString += '@ds135252.mlab.com:35252/heroku_krl15f78'; // user yours
     }
 
     var mongoose = require("mongoose");
